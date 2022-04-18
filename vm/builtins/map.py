79b2006c11a6035eb87c5b4a5c0a42e2f23b4f66
Yair Twito <yairt16@gmail.com>
2022-04-18 13:46:12 +0300

    impl builtins.map

--- vm/builtins/map.py
+++ vm/builtins/map.py
@@ -1,19 +1,36 @@
 from __future__ import annotations
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
+
+
 if TYPE_CHECKING:
     from vm.builtins.pytype import PyTypeRef
-    from vm.pyobject import PyClassImpl, PyContext, PyValueMixin, pyclass, pyimpl, tp_flags
-    from vm.pyobjectrc import PyObjectRef
+    from vm.pyobject import PyContext
+    from vm.pyobjectrc import PyObjectRef, PyRef
     from protocol.iter import PyIter
     from vm.vm import VirtualMachine
+
+    # from vm.function_ import FuncArgs
+
 import vm.pyobject as po
+import vm.types.slot as slot
+import vm.protocol.iter as protocol_iter
+import vm.function_ as fn
+
+from common.error import unreachable
+
 
 @po.tp_flags(basetype=True)
 @po.pyimpl(constructor=True, iter_next=True)
 @po.pyclass("map")
 @dataclass
-class PyMap(po.PyClassImpl, po.PyValueMixin):
+class PyMap(
+    po.PyClassImpl,
+    po.PyValueMixin,
+    slot.ConstructorMixin,
+    slot.IterNextIterableMixin,
+    slot.IterNextMixin,
+):
     mapper: PyObjectRef
     iterators: list[PyIter]
 
@@ -21,9 +38,34 @@ class PyMap(po.PyClassImpl, po.PyValueMixin):
     def class_(cls, vm: VirtualMachine) -> PyTypeRef:
         return vm.ctx.types.map_type
 
-    # TODO: impl Constructor for PyMap
-    # TODO: impl IterNextIterable for PyMap
-    # TODO: impl IterNext for PyMap
+    @classmethod
+    def py_new(
+        cls, class_: PyTypeRef, fargs: fn.FuncArgs, /, vm: VirtualMachine
+    ) -> PyObjectRef:
+        args = fargs.bind(__py_new).arguments
+        return PyMap(
+            mapper=args["mapper"], iterators=list(args["iterators"])
+        ).into_pyresult_with_type(vm, class_)
+
+    @classmethod
+    def next(cls, zelf: PyRef[PyMap], vm: VirtualMachine) -> protocol_iter.PyIterReturn:
+        next_objs = []
+        for iterator in zelf._.iterators:
+            v = iterator.next(vm)
+            if isinstance(v, protocol_iter.PyIterReturnReturn):
+                item = v.value
+            elif isinstance(v, protocol_iter.PyIterReturnStopIteration):
+                return v
+            else:
+                unreachable()
+            next_objs.append(item)
+        return protocol_iter.PyIterReturn.from_pyresult(
+            lambda: vm.invoke(zelf._.mapper, fn.FuncArgs(next_objs)), vm
+        )
+
+
+def __py_new(mapper, /, *iterators):
+    ...
 
 
 def init(context: PyContext) -> None:
