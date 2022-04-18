4d07b3b96d4444ac94f7f9f0cda6379890f877ce
Yair Twito <yairt16@gmail.com>
2022-04-18 14:10:05 +0300

    impl builtins.filter

--- vm/builtins/filter.py
+++ vm/builtins/filter.py
@@ -1,19 +1,33 @@
 from __future__ import annotations
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
+from common.error import unreachable
+
+
 if TYPE_CHECKING:
     from vm.builtins.pytype import PyTypeRef
     from vm.pyobject import PyContext
-    from vm.pyobjectrc import PyObjectRef
+    from vm.pyobjectrc import PyObjectRef, PyRef
     from protocol.iter import PyIter
     from vm.vm import VirtualMachine
+import vm.function_ as fn
 import vm.pyobject as po
+import vm.types.slot as slot
+import vm.protocol.iter as protocol_iter
+
 
 @po.tp_flags(basetype=True)
 @po.pyimpl(iter_next=True, constructor=True)
 @po.pyclass("filter")
 @dataclass
-class PyFilter(po.PyValueMixin, po.PyClassImpl):
+class PyFilter(
+    po.PyValueMixin,
+    po.PyClassImpl,
+    po.TryFromObjectMixin,
+    slot.IterNextMixin,
+    slot.IterNextIterableMixin,
+    slot.ConstructorMixin,
+):
     predicate: PyObjectRef
     iterator: PyIter
 
@@ -21,10 +35,49 @@ class PyFilter(po.PyValueMixin, po.PyClassImpl):
     def class_(cls, vm: VirtualMachine) -> PyTypeRef:
         return vm.ctx.types.filter_type
 
-    # TODO: impl Constructor for PyFilter
+    @classmethod
+    def py_new(
+        cls, class_: PyTypeRef, fargs: fn.FuncArgs, /, vm: VirtualMachine
+    ) -> PyObjectRef:
+        args = fargs.bind(__py_new).arguments
+        return PyFilter(
+            predicate=args["predicate"], iterator=args["iterator"]
+        ).into_pyresult_with_type(vm, class_)
 
-    # TODO: impl IterNextIterable for PyFilter
-    # TODO: impl IterNext for PyFilter
+    @classmethod
+    def next(
+        cls, zelf: PyRef[PyFilter], vm: VirtualMachine
+    ) -> protocol_iter.PyIterReturn:
+        predicate = zelf._.predicate
+        while 1:
+            v = zelf._.iterator.next(vm)
+            if isinstance(v, protocol_iter.PyIterReturnReturn):
+                next_obj = v.value
+            elif isinstance(v, protocol_iter.PyIterReturnStopIteration):
+                return v
+            else:
+                unreachable()
+            predicate_value: PyObjectRef
+            if vm.is_none(predicate):
+                predicate_value = next_obj
+            else:
+                r = protocol_iter.PyIterReturn.from_pyresult(
+                    lambda: vm.invoke(predicate, fn.FuncArgs([next_obj])), vm
+                )
+                if isinstance(r, protocol_iter.PyIterReturnReturn):
+                    predicate_value = r.value
+                elif isinstance(r, protocol_iter.PyIterReturnStopIteration):
+                    return r
+                else:
+                    unreachable()
+
+            if predicate_value.try_to_bool(vm):
+                return protocol_iter.PyIterReturnReturn(next_obj)
+        unreachable()
+
+
+def __py_new(predicate, iterator, /):
+    ...
 
 
 def init(context: PyContext) -> None:
